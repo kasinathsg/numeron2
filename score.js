c4e214492e1db8cea9d1871933f3dfa9532db685
kasinathsg <108450699+kasinathsg@users.noreply.github.com>
2022-11-03 14:57:38 +0530

    Add files via upload

---
+++ score.js
@@ -0,0 +1,9 @@
+// Iteration 8: Making scoreboard functional
+var playAgain = document.getElementById('play-again-button')
+
+playAgain.onclick = function(){
+    location.href = 'game.html'
+}
+
+var updatescore = document.getElementById('score-board')
+updatescore.innerHTML =sessionStorage.getItem("score")
